5c7cccaeb6e33e6b4c815e1ab6469a626d3bf995
MariaGraziaSicuro <merygsicuro@gmail.com>
2023-02-14 10:48:47 +0100

    g5-Test

---
+++ m1/s2/g5/calcolatrice.js
@@ -0,0 +1,17 @@
+
+
+function aggiungiCifra(bottone){
+
+ document.getElementById("numeri").value += bottone.getAttribute('data-simbolo');
+    
+}
+    
+    
+    
+function tot(){
+    
+ let numeri = document.getElementById("numeri");
+ numeri.value = eval(numeri.value);
+}
+    
+    
